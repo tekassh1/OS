30813efd653d3700061414c386f6e4875a458e0e
kuznecov <kuznecov@protei.ru>
2024-11-26 01:16:11 +0300

    .gitignore added.

--- Lab1/include/shell.h
+++ Lab1/include/shell.h
@@ -4,13 +4,20 @@
 #include <vector>
 #include <string>
 
+#define STACK_SIZE 1024*1024
+
 class Shell {
 private:
     char* cwd = nullptr;
 
-    int change_dir(std::vector<std::string>& argv);
+    int changeDir(std::vector<std::string>& argv);
     std::vector<std::string> split(const std::string& s, char delim);
 
+    static int executeProcess(void* args);
+    static int childProcess(void* args);
+
+    static char** stringArrayToCharArray(std::vector<std::string> arr);
+    static void clearDynaArgs(char** argv, size_t size);
 public:
     int start();
 };
--- Lab1/src/Shell/shell.cpp
+++ Lab1/src/Shell/shell.cpp
@@ -1,41 +1,105 @@
 #include <unistd.h>
 #include <iostream>
 
+#include <csignal>
+#include <sys/wait.h>
+#include <cstring>
+#include <sstream>
+
 #include "shell.h"
 
+std::vector<std::string> Shell::split(const std::string& s, char delim) {
+    std::vector<std::string> tokens;
+    std::string token;
+    std::stringstream tokenStream(s);
+
+    while (std::getline(tokenStream, token, delim)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+char** Shell::stringArrayToCharArray(std::vector<std::string> arr) {
+    char** res = new char*[arr.size() + 1];
+    for (int i = 0; i < arr.size(); i++) {
+        res[i] = new char[arr[i].size() + 1];
+        strcpy(res[i], arr[i].c_str());
+    }
+    res[arr.size()] = nullptr;
+    return res;
+}
+
+void Shell::clearDynaArgs(char** argv, size_t size) {
+    for (int i = 0; i < size; i++) {
+        delete[] argv[i];
+    }
+    delete[] argv;
+}
+
 int Shell::start() {
-    char buff[256];
-    cwd = getcwd(buff, 256);
+    cwd = getcwd(nullptr, 256);
 
     while (true) {
-        std::cout << buff << " > ";
-        std::string input; std::cin >> input;
+        std::cout << cwd << " > ";
+        std::string input;
+        std::getline(std::cin, input);
 
-        std::vector<std::string> argv = split(input, ' ');
+        std::vector<std::string> argv_s = split(input, ' ');
 
-        if (argv.size() < 1)
+        if (argv_s.empty())
             continue;
 
-        if (argv[0] == "exit") {
+        if (argv_s[0] == "exit") {
             return 0;
         }
-        else if (argv[0] == "cd") {
-            int res = change_dir(argv);
+        else if (argv_s[0] == "cd") {
+            int res = changeDir(argv_s);
             if (res != 0) {
-                std::cout << "Wrong path!" << "\n";
+                perror("cd");
             }
         }
         else {
-
+            char** argv = stringArrayToCharArray(argv_s);
+            executeProcess(argv);
+            clearDynaArgs( argv, argv_s.size());
         }
     }
 }
 
-int Shell::change_dir(std::vector<std::string> &argv) {
+int Shell::changeDir(std::vector<std::string> &argv) {
     int res = chdir(argv[1].c_str());
-    if (res != 0) {
-        char buff[256];
-        cwd = getcwd(buff, 256);
+    if (res == 0) {
+        cwd = getcwd(nullptr, 256);
     }
+    return res;
+}
+
+int Shell::childProcess(void* arg) {
+    char** args = static_cast<char**>(arg);
+
+    if (execvp(args[0], args) == -1) {
+        if (errno == ENOENT) {
+            std::cerr << "Unknown command: " << args[0] << std::endl;
+        } else {
+            perror("execvp");
+        }
+        _exit(EXIT_FAILURE);
+    }
+}
+
+
+int Shell::executeProcess(void* arg) {
+    char* child_stack = new char[STACK_SIZE];
+    pid_t pid = clone(childProcess, child_stack + STACK_SIZE, SIGCHLD, arg);
+
+    pid_t res = -1;
+    if (pid == -1) {
+        perror("clone");
+    }
+    else {
+        res = waitpid(pid, nullptr, 0);
+    }
+
+    delete[] child_stack;
     return res;
 }
